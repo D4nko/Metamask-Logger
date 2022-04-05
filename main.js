f8c70925c05fc828b73489a53813917024eb6589
D4nko <75887653+D4nko@users.noreply.github.com>
2022-04-05 14:10:26 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+/* Moralis init code */
+const serverUrl = "psate your serverUrl";
+const appId = "psate your appId";
+Moralis.start({ serverUrl, appId });
+
+/* Authentication code */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate({
+      signingMessage: "Login using Moralis API",
+    })
+      .then(function (user) {
+        console.log("logged in user:", user);
+        console.log(user.get("ethAddress"));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
